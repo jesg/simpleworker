1c3ff908f540ad57d80c8adacffde3dc55ce90fa
jesg <gowanjason@gmail.com>
2014-11-07 04:12:28 -0600

    delete workers from simpleworker.rb

--- lib/simpleworker.rb
+++ lib/simpleworker.rb
@@ -12,8 +12,6 @@ end
 require 'simpleworker/redis_support'
 require 'simpleworker/runner'
 require 'simpleworker/abstract_worker'
-require 'simpleworker/local_worker'
-require 'simpleworker/ssh_worker'
 require 'simpleworker/abstract_listener'
 require 'simpleworker/event_monitor'
 require 'simpleworker/event_server'
