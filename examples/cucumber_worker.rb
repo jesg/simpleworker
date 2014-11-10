87ef228ffb41d0b5b211b9b61bfc5a8d88231aac
jesg <gowanjason@gmail.com>
2014-11-10 04:53:52 -0600

    add cucumber example

---
+++ examples/cucumber_worker.rb
@@ -0,0 +1,20 @@
+require 'simpleworker'
+require 'cucumber/cli/main'
+
+redis      = Redis.new
+task_queue = SimpleWorker::TaskQueue.new(redis, 'my_hostname', ENV['JOBID'])
+
+task_queue.fire_start
+
+task_queue.each_task do |task|
+  status = nil
+  begin
+    Cucumber::Cli::Main.execute task
+  rescue SystemExit => e
+    status = e.success?
+  end
+
+  fire_log_message "Cucumber task: #{task} status: #{status}"
+end
+
+task_queue.fire_stop
