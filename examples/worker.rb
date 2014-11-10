b355e065df95719ec11e12fde8bbe571c907d1e1
jesg <gowanjason@gmail.com>
2014-11-10 04:23:53 -0600

    update example to use log message

--- examples/worker.rb
+++ examples/worker.rb
@@ -6,7 +6,7 @@ task_queue = SimpleWorker::TaskQueue.new(redis, 'my_hostname', ENV['JOBID'])
 task_queue.fire_start
 
 task_queue.each_task do |task|
-  puts "Task: #{task}"
+  task_queue.fire_log_message "Do Task: #{task}"
 end
 
 task_queue.fire_stop
